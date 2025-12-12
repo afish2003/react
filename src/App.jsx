0e43cfc2a5fe9aeb07fe05c1121c87870053a7a3
Alexander Fisher <101442344+afish2003@users.noreply.github.com>
2025-12-12 14:54:44 -0500

    add FakeScanner route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home.jsx";
 import ChaosButton from "./pages/chaosButton.jsx";
+import FakeScanner from "./pages/fakeScanner.jsx";
 
 export default function App() {
     return (
@@ -8,6 +9,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/chaosButton" element={<ChaosButton />} />
+                <Route path="/fakeScanner" element={<FakeScanner />} />
             </Routes>
         </HashRouter>
     );
